c9a968e735e7c0cb315185f0f88b11519a7a7012
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-31 04:23:42 +0000

    Merge code from frontend

--- components/simple_cta_component_1722399660.js
+++ components/simple_cta_component_1722399660.js
@@ -14,7 +14,7 @@ Vue.component("simple_cta_component_1722399660", {
                 </div>
                 
                 <div id="cta-button-container" class="flex">
-                    <a id="cta-button" href="#" class="flex-1 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">STARTER_TEMPLATE_CALL_TO_ACTION_Great</a>
+                    <a id="cta-button" href="#" class="flex-1 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Powerful Great</a>
                 </div>
             </div>
         </div>
